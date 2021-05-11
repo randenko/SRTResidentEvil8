74d7d45c0c3e4abe70137e7fdeb190088fd2681b
Christopher D Couture <videogameroulette@yahoo.ca>
2021-05-11 14:48:03 -0700

    commit 1.0.0.2

--- index.js
+++ index.js
@@ -40,29 +40,31 @@ function appendData(data) {
 	mainContainer.innerHTML = "";
 
 	//PLAYERS HP
-	var hitPercent = (data.CurrentHP / data.MaxHP) * 100;
+	var hitPercent = (data.PlayerCurrentHealth / data.PlayerMaxHealth) * 100;
 	if (hitPercent > 66) {
 		mainContainer.innerHTML += `<div class="hp"><div class="hpbar fine" style="width:${hitPercent}%">
-				<div id="currenthp">${data.CurrentHP}</div><div class="green" id="percenthp">${hitPercent.toFixed(1)}%</div></div></div>`;
+				<div id="currenthp">${data.PlayerCurrentHealth}</div><div class="green" id="percenthp">${hitPercent.toFixed(1)}%</div></div></div>`;
 	}
 	else if (hitPercent <= 66 && hitPercent > 33) {
 		mainContainer.innerHTML += `<div class="hp"><div class="hpbar caution" style="width:${hitPercent}%">
-				<div id="currenthp">${data.CurrentHP}</div><div class="yellow" id="percenthp">${hitPercent.toFixed(1)}%</div></div></div>`;
+				<div id="currenthp">${data.PlayerCurrentHealth}</div><div class="yellow" id="percenthp">${hitPercent.toFixed(1)}%</div></div></div>`;
 	}
 	else if (hitPercent <= 33 && hitPercent > 0){
 		mainContainer.innerHTML += `<div class="hp"><div class="hpbar danger" style="width:${hitPercent}%">
-				<div id="currenthp">${data.CurrentHP}</div><div class="red" id="percenthp">${hitPercent.toFixed(1)}%</div></div></div>`;
+				<div id="currenthp">${data.PlayerCurrentHealth}</div><div class="red" id="percenthp">${hitPercent.toFixed(1)}%</div></div></div>`;
 	}
 	else {
 		mainContainer.innerHTML += `<div class="hp"><div class="hpbar dead" style="width:${100}%">
-				<div id="currenthp">${data.CurrentHP}</div><div class="grey" id="percenthp">${0}%</div></div></div>`;
+				<div id="currenthp">${data.PlayerCurrentHealth}</div><div class="grey" id="percenthp">${0}%</div></div></div>`;
 	}
 	
-	mainContainer.innerHTML += `<font size="4" color="#fff"><div id="lei">Lei: ${data.CurrentLei}</div></font>`;
+	mainContainer.innerHTML += `<font size="4" color="#fff"><div id="lei">Lei: ${data.Lei}</div></font>`;
 	
-	mainContainer.innerHTML += `<div id="darank">DA Rank: ${data.CurrentDARank}</div>`;
+	mainContainer.innerHTML += `<div id="darank">DA Rank: ${data.Rank}</div>`;
 
-	mainContainer.innerHTML += `<div id="score">DA Score: ${data.CurrentDAScore}</div>`;
+	mainContainer.innerHTML += `<div id="score">DA Score: ${data.RankScore}</div>`;
+
+	mainContainer.innerHTML += `<div id="gi">Game Init: ${data.GameInit}</div>`;
 
 	mainContainer.innerHTML += `<div id="gs">Game State: ${data.GameState}</div>`;
 
@@ -73,4 +75,17 @@ function appendData(data) {
 	mainContainer.innerHTML += `<div id="cs2">Cutscene Playing: ${cid}</div>`;
 
 	mainContainer.innerHTML += `<div id="ps">Paused: ${data.PauseState}</div>`;
+
+	if (data.LadyD != null) {
+		if (data.LadyD.IsAlive)
+		{
+			var ldHitPercent = (data.LadyD.CurrentHP / data.LadyD.MaximumHP) * 100;
+			mainContainer.innerHTML += `<div class="enemyhp"><div class="enemyhpbar fine" style="width:${ldHitPercent}%">
+				<div id="currentenemyhp">${data.LadyD.CurrentHP}</div><div class="green" id="percentenemyhp">${ldHitPercent.toFixed(1)}%</div></div></div>`;
+		}
+	}
+	//else {
+	//	mainContainer.innerHTML += `<div class="enemyhp"><div class="enemyhpbar danger" style="width:100%">
+	//			<div id="currentenemyhp">Lady Dimitrescu: 9000</div><div class="red" id="percentenemyhp">100.0%</div></div></div>`;
+	//}
 }
