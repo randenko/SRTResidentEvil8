0c64878c3f31dab527e51dbf18a7d9e40d70421c
Christopher D Couture <videogameroulette@yahoo.ca>
2021-05-13 03:22:04 -0700

    commit 1.0.0.6
    all bosses added and more enemy hp logic

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const POLLING_RATE = 1000;
 
 const JSON_ENDPOINT = `http://${JSON_ADDRESS}:${JSON_PORT}/`;
 
+var KilledBosses = [];
+
 window.onload = function () {
 	getData();
 	setInterval(getData, POLLING_RATE);
@@ -38,7 +40,9 @@ function appendData(data) {
 	//console.log(data);
 	var mainContainer = document.getElementById("srtQueryData");
 	mainContainer.innerHTML = "";
-
+	if (data.GameState == 64) {
+		ClearBosses();
+	}
 	//PLAYERS HP
 	var hitPercent = (data.PlayerCurrentHealth / data.PlayerMaxHealth) * 100;
 	if (hitPercent > 66) {
@@ -76,27 +80,45 @@ function appendData(data) {
 
 	mainContainer.innerHTML += `<div id="ps">Paused: ${data.PauseState}</div>`;
 
-	GetBoss(data.Urias[0], "Urias", 25000);
-	GetBoss(data.Sisters, "Bella", 2900);
-	GetBoss(data.Sisters, "Cassandra", 3400);
-	GetBoss(data.Sisters, "Daniella", 3700);
-	GetBoss(data.LadyD, "Lady Dimitrescu", 9000);
-	GetBoss(data.Moreau, "Moreau", 26000);
-	GetBoss(data.Urias[1], "Urias", 25000);
-	//GetBoss(data.Urias[2], "Urias Strǎjer", 75000);
-	//GetBoss(data.Sturm, "Sturm", 15000);
-	//GetBoss(data.Heisenberg, "Heisenberg", 100000);
-	//GetBoss(data.Miranda, "Miranda", 30000);
-	//GetBoss(data.Miranda, "Miranda2", 100000);
+	GetBoss(data.Boss, "Urias", 25000);
+	GetBoss(data.Boss, "Bella", 2900);
+	GetBoss(data.Boss, "Cassandra", 3400);
+	GetBoss(data.Boss, "Daniella", 3700);
+	GetBoss(data.Boss, "Lady Dimitrescu", 9000);
+	GetBoss(data.Boss, "Moreau", 26000);
+	GetBoss(data.Boss, "Urias", 25000);
+	GetBoss(data.Boss, "Urias Strǎjer", 75000);
+	GetBoss(data.Boss, "Sturm", 15000);
+	GetBoss(data.Boss, "Heisenberg", 100000);
+	GetBoss(data.Boss, "Miranda", 30000);
 }
 
 function GetBoss(boss, name, max) {
 	let mainContainer = document.getElementById("srtQueryData");
-	if (boss.IsAlive)
+	//console.log(KilledBosses);
+	if (boss.IsAlive && !KilledBosses.includes(name))
 	{
 		if (boss.MaximumHP != max) { return; }
 		let _HitPercent = (boss.CurrentHP / boss.MaximumHP) * 100;
 		mainContainer.innerHTML += `<div class="enemyhp"><div class="enemyhpbar danger" style="width:${_HitPercent}%">
 			<div id="currentenemyhp">${name}: ${boss.CurrentHP}</div><div class="red" id="percentenemyhp">${_HitPercent.toFixed(1)}%</div></div></div>`;
+	}
+	else {
+		if (boss.MaximumHP == max && boss.CurrentHP == 0)
+		{
+			if (KilledBosses.includes(name)) { return; }
+			KilledBosses.push(name);
+			console.log(`Boss ${name} Has Been Killed.`);
+			console.log(KilledBosses);
+		}
+	}
+}
+
+function ClearBosses()
+{
+	if (KilledBosses.length > 0)
+	{
+		KilledBosses = [];
+		console.log("Killed Boss Array Cleared");
 	}
 }
