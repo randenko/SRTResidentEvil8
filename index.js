dca19573ca933d7090987133f086a2d54b6a4b33
Christopher D Couture <videogameroulette@yahoo.ca>
2021-05-16 07:31:55 -0700

    commit 1.0.0.7
    fixed enemy hp issues all enemies shown now that are damaged

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const POLLING_RATE = 1000;
 
 const JSON_ENDPOINT = `http://${JSON_ADDRESS}:${JSON_PORT}/`;
 
-var KilledBosses = [];
-
 window.onload = function () {
 	getData();
 	setInterval(getData, POLLING_RATE);
@@ -34,6 +32,40 @@ function getData() {
 		.catch(function (err) {
 			console.log("Error: " + err);
 		});
+}
+
+function GetGameInit(data) {
+	switch (data.GameInit)
+	{
+		case 1:
+			return "None";
+		case 2:
+			return "Running";
+		case 4:
+			return "PlayerPaused";
+		default:
+			return data.GameInit;
+	}
+}
+
+function GetGameState(data) {
+	switch (data.GameState)
+	{
+		case 0:
+			return "None";
+		case 1:
+			return "Paused";
+		case 2:
+			return "LoadComplete";
+		case 3:
+			return "Loading";
+		case 64:
+			return "FirstCutscene";
+		case 65:
+			return "FirstCutscenePaused";
+		default:
+			return data.GameState;
+	}
 }
 
 function appendData(data) {
@@ -68,57 +100,31 @@ function appendData(data) {
 
 	mainContainer.innerHTML += `<div id="score">DA Score: ${data.RankScore}</div>`;
 
-	mainContainer.innerHTML += `<div id="gi">Game Init: ${data.GameInit}</div>`;
+	mainContainer.innerHTML += `<div id="gi">Game Init: ${GetGameInit(data)}</div>`;
 
-	mainContainer.innerHTML += `<div id="gs">Game State: ${data.GameState}</div>`;
+	mainContainer.innerHTML += `<div id="gs">Game State: ${GetGameState(data)}</div>`;
 
 	var cid = (data.CutsceneState != 4294967295 || data.CutsceneID != 4294967295) ? true : false;
 
-	mainContainer.innerHTML += `<div id="cs">Cutscene State: ${data.CutsceneState} | ${data.CutsceneID}</div>`;
+	//mainContainer.innerHTML += `<div id="cs">Cutscene State: ${data.CutsceneState} | ${data.CutsceneID}</div>`;
 
 	mainContainer.innerHTML += `<div id="cs2">Cutscene Playing: ${cid}</div>`;
 
-	mainContainer.innerHTML += `<div id="ps">Paused: ${data.PauseState}</div>`;
+	var pstate = (data.PauseState == 1) ? true : false;
 
-	GetBoss(data.Boss, "Urias", 25000);
-	GetBoss(data.Boss, "Bella", 2900);
-	GetBoss(data.Boss, "Cassandra", 3400);
-	GetBoss(data.Boss, "Daniella", 3700);
-	GetBoss(data.Boss, "Lady Dimitrescu", 9000);
-	GetBoss(data.Boss, "Moreau", 26000);
-	GetBoss(data.Boss, "Urias", 25000);
-	GetBoss(data.Boss, "Urias Strǎjer", 75000);
-	GetBoss(data.Boss, "Sturm", 15000);
-	GetBoss(data.Boss, "Heisenberg", 100000);
-	GetBoss(data.Boss, "Miranda", 30000);
-}
+	mainContainer.innerHTML += `<div id="ps">Is Paused: ${pstate}</div>`;
 
-function GetBoss(boss, name, max) {
-	let mainContainer = document.getElementById("srtQueryData");
-	//console.log(KilledBosses);
-	if (boss.IsAlive && !KilledBosses.includes(name))
-	{
-		if (boss.MaximumHP != max) { return; }
-		let _HitPercent = (boss.CurrentHP / boss.MaximumHP) * 100;
-		mainContainer.innerHTML += `<div class="enemyhp"><div class="enemyhpbar danger" style="width:${_HitPercent}%">
-			<div id="currentenemyhp">${name}: ${boss.CurrentHP}</div><div class="red" id="percentenemyhp">${_HitPercent.toFixed(1)}%</div></div></div>`;
-	}
-	else {
-		if (boss.MaximumHP == max && boss.CurrentHP == 0)
-		{
-			if (KilledBosses.includes(name)) { return; }
-			KilledBosses.push(name);
-			console.log(`Boss ${name} Has Been Killed.`);
-			console.log(KilledBosses);
+	//var table = document.createElement("table");
+	var filterdEnemies = data.EnemyHealth.filter(m => { return m.IsAlive });
+	//console.log("Filtered Enemies", filterdEnemies);
+	filterdEnemies.sort(function (a, b) {
+		return Asc(a.Percentage, b.Percentage) || Desc(a.CurrentHP, b.CurrentHP);
+	}).forEach(function (item, index, arr) {
+		if (item.IsAlive) {
+			mainContainer.innerHTML += `<div class="enemyhp"><div class="enemyhpbar danger" style="width:${(item.Percentage * 100).toFixed(1)}%">
+			<div id="currentenemyhp">${item.CurrentHP}</div><div class="red" id="percentenemyhp">${(item.Percentage * 100).toFixed(1)}%</div></div></div>`;
 		}
-	}
-}
-
-function ClearBosses()
-{
-	if (KilledBosses.length > 0)
-	{
-		KilledBosses = [];
-		console.log("Killed Boss Array Cleared");
-	}
+		
+	});
+	//mainContainer.appendChild(table);
 }
