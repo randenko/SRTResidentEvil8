2cdeb3823aff7bd515434e236b6d26dbe7590a61
VideoGameRoulette <videogameroulette@yahoo.ca>
2021-05-21 23:57:35 -0400

    Update 1.0.1

--- index.js
+++ index.js
@@ -38,27 +38,29 @@ function GetGameInit(data) {
 	let mainContainer = document.getElementById("srtQueryData");
 	if (data.CurrentView.includes("Chapter"))
 	{
-		mainContainer.innerHTML += `<div id="gi">Game Init: <font size="4" color="#00FF00">True</font></div>`;
+		mainContainer.innerHTML += `<div id="gi">Game Init: <font color="#00FF00">True</font></div>`;
 		return;
 	}
-	mainContainer.innerHTML += `<div id="gi">Game Init: <font size="4" color="#FF0000">False</font></div>`;
+	mainContainer.innerHTML += `<div id="gi">Game Init: <font color="#FF0000">False</font></div>`;
 }
 
 function GetGameState(data) {
 	switch (data.GameState)
 	{
 		case 0:
-			return "None";
+			return "Default";
 		case 1:
 			return "Paused";
 		case 2:
-			return "LoadComplete";
+			return "Loading";
 		case 3:
 			return "Loading";
+		case 32:
+			return "Inventory / Map";
 		case 64:
-			return "FirstCutscene";
+			return "Village Of Shadows";
 		case 65:
-			return "FirstCutscenePaused";
+			return "Village Of Shadows Paused";
 		default:
 			return data.GameState;
 	}
@@ -66,22 +68,28 @@ function GetGameState(data) {
 
 function IsCutscene(data) {
 	let mainContainer = document.getElementById("srtQueryData");
-	if (data.CutsceneState != 4294967295 || data.CutsceneID != 4294967295)
+	if (data.CutsceneTimer != 0)
 	{
-		mainContainer.innerHTML += `<div id="cs2">Cutscene Playing: <font size="4" color="#00FF00">True</font></div>`;
+		mainContainer.innerHTML += `
+		<div id="cs2">
+			<div class="title">Cutscene Playing: </div><font color="#00FF00">True</font>
+		</div>`;
 		return;
 	}
-	mainContainer.innerHTML += `<div id="cs2">Cutscene Playing: <font size="4" color="##FF0000">False</font></div>`;
+	mainContainer.innerHTML += `
+	<div id="cs2">
+		<div class="title">Cutscene Playing: </div><font color="##FF0000">False</font>
+	</div>`;
 }
 
 function IsPaused(data) {
 	let mainContainer = document.getElementById("srtQueryData");
 	if (data.PauseState == 1)
 	{
-		mainContainer.innerHTML += `<div id="ps">Is Paused: <font size="4" color="#00FF00">True</font></div>`;
+		mainContainer.innerHTML += `<div id="ps">Is Paused: <font color="#00FF00">True</font></div>`;
 		return;
 	}
-	mainContainer.innerHTML += `<div id="ps">Is Paused: <font size="4" color="#FF0000">False</font></div>`;
+	mainContainer.innerHTML += `<div id="ps">Is Paused: <font color="#FF0000">False</font></div>`;
 }
 
 function GetPlayerHP(data) {
@@ -105,30 +113,77 @@ function GetPlayerHP(data) {
 	}
 }
 
+function GetCurrentChapter(data) {
+	let mainContainer = document.getElementById("srtQueryData");
+	if (data.CurrentChapter == "")
+	{
+		mainContainer.innerHTML += `
+		<div id="chapter">
+			<div class="title">Current Chapter: </div><font color="#FF0000">None</font>
+		</div>`;
+		return;
+	}
+	mainContainer.innerHTML += `
+	<div id="chapter">
+		<div class="title">Current Chapter: </div><font color="#00FF00">${data.CurrentChapter}</font>
+	</div>`;
+}
+
+function GetCurrentRoom(data) {
+	let mainContainer = document.getElementById("srtQueryData");
+	if (data.CurrentRoom == "")
+	{
+		mainContainer.innerHTML += `
+		<div id="room">
+			<div class="title">Current Room: </div><font color="#FF0000">None</font>
+		</div>`;
+		return;
+	}
+	mainContainer.innerHTML += `
+	<div id="room">
+		<div class="title">Current Room: </div><font color="#00FF00">${data.CurrentRoom.split('\0')[0]}</font>
+	</div>`;
+}
+
 function appendData(data) {
 	//console.log(data);
 	var mainContainer = document.getElementById("srtQueryData");
 	mainContainer.innerHTML = "";
+
+	mainContainer.innerHTML += `
+	<div id="position">
+		X: <font color="#00FF00">${data.PlayerPositionX.toFixed(3)}</font>
+		Y: <font color="#00FF00">${data.PlayerPositionY.toFixed(3)}</font>
+		Z: <font color="#00FF00">${data.PlayerPositionZ.toFixed(3)}</font>
+	</div>`;
+
 	//PLAYERS HP
 	GetPlayerHP(data);
-	
-	mainContainer.innerHTML += `<div id="chapter">Current Chapter: <font size="4" color="#00FF00">${data.CurrentChapter}</font></div>`;
-	
-	mainContainer.innerHTML += `<div id="position">X: <font size="4" color="#00FF00">${data.PlayerPositionX.toFixed(3)}</font>- Y: <font size="4" color="#00FF00">${data.PlayerPositionY.toFixed(3)}</font> - Z: <font size="4" color="#00FF00">${data.PlayerPositionZ.toFixed(3)}</font></div>`;
 
-	mainContainer.innerHTML += `<div id="da">DA Rank: <font size="4" color="#00FF00">${data.Rank}</font> DA Score: <font size="4" color="#00FF00">${data.RankScore}</font></div>`;
+	mainContainer.innerHTML += `
+	<div id="da">
+		<div class="title">DA Score: </div><font color="#00FF00">${data.RankScore}</font> 
+		<div class="title">DA Rank: </div><font color="#00FF00">${data.Rank}</font>
+	</div>`;
 
-	mainContainer.innerHTML += `<div id="lei">Lei: <font size="4" color="#00FF00">${data.Lei}</font></div>`;
+	GetCurrentChapter(data);
 
-	GetGameInit(data);
+	GetCurrentRoom(data);
 
-	mainContainer.innerHTML += `<div id="gs">Game State: <font size="4" color="#00FF00">${GetGameState(data)}</font></div>`;
+	mainContainer.innerHTML += `
+	<div id="gs">
+		<div class="title">Game State: </div><font color="#00FF00">${GetGameState(data)}</font>
+	</div>`;
 
 	IsCutscene(data);	
-	IsPaused(data);
+
+	mainContainer.innerHTML += `
+	<div id="lei">
+		<div class="title">Lei: </div><font color="#00FF00">${data.Lei}</font>
+	</div>`;
 
 	//var table = document.createElement("table");
-	var filterdEnemies = data.EnemyHealth.filter(m => { return m.IsAlive });
+	var filterdEnemies = data.EnemyHealth.filter(m => { return (m.IsAlive) });
 	//console.log("Filtered Enemies", filterdEnemies);
 	filterdEnemies.sort(function (a, b) {
 		return Asc(a.Percentage, b.Percentage) || Desc(a.CurrentHP, b.CurrentHP);
@@ -137,7 +192,6 @@ function appendData(data) {
 			mainContainer.innerHTML += `<div class="enemyhp"><div class="enemyhpbar danger" style="width:${(item.Percentage * 100).toFixed(1)}%">
 			<div id="currentenemyhp">${item.CurrentHP}</div><div class="red" id="percentenemyhp">${(item.Percentage * 100).toFixed(1)}%</div></div></div>`;
 		}
-		
 	});
 	//mainContainer.appendChild(table);
 }
