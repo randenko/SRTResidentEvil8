9bb1e0e74a109b1854430942d9f5c2005025d219
VideoGameRoulette <videogameroulette@yahoo.ca>
2021-06-01 09:14:18 -0400

    update 1.0.1.5b

--- index.js
+++ index.js
@@ -4,7 +4,35 @@ const POLLING_RATE = 333;
 
 const JSON_ENDPOINT = `http://${JSON_ADDRESS}:${JSON_PORT}/`;
 
+var HideCurrentEvent = false;
+var HidePlayerPosition = false;
+var HideStats = false;
+var HideEnemies = false;
+var HideDebug = false;
+
 window.onload = function () {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	const event = urlParams.get('event');
+	if (event != null) {
+		HideCurrentEvent = true;
+	}
+	const position = urlParams.get('position');
+	if (position != null) {
+		HidePlayerPosition = true;
+	}
+	const stats = urlParams.get('stats');
+	if (stats != null) {
+		HideStats = true;
+	}
+	const enemy = urlParams.get('enemy');
+	if (enemy != null) {
+		HideEnemies = true;
+	}
+	const debug = urlParams.get('debug');
+	if (debug != null) {
+		HideDebug = true;
+	}
 	getData();
 	setInterval(getData, POLLING_RATE);
 };
@@ -37,7 +65,7 @@ function getData() {
 function GetPlayerHP(data) {
 	let mainContainer = document.getElementById("srtQueryData");
 	var hitPercent = (data.PlayerCurrentHealth / data.PlayerMaxHealth) * 100;
-	var playerName = (data.PlayerStatus.IsEthan) ? "Ethan: " : (data.PlayerStatus.IsEthan) ? "Chris: " : "";
+	var playerName = (data.PlayerStatus.IsEthan) ? "Ethan: " : (data.PlayerStatus.IsChris) ? "Chris: " : "";
 	if (hitPercent > 66) {
 		mainContainer.innerHTML += `<div class="hp"><div class="hpbar fine" style="width:${hitPercent}%">
 				<div id="currenthp">${playerName}${data.PlayerCurrentHealth} / ${data.PlayerMaxHealth}</div><div class="green" id="percenthp">${hitPercent.toFixed(1)}%</div></div></div>`;
@@ -58,7 +86,7 @@ function GetPlayerHP(data) {
 
 function GetCurrentEvent(data) {
 	let mainContainer = document.getElementById("srtQueryData");
-	if (data.CurrentEvent == undefined)
+	if (data.CurrentEvent == undefined || HideCurrentEvent)
 	{
 		return;
 	}
@@ -76,31 +104,40 @@ function GetCurrentEvent(data) {
 	</div>`;
 }
 
-function appendData(data) {
-	//console.log(data);
-	var mainContainer = document.getElementById("srtQueryData");
-	mainContainer.innerHTML = "";
-
-	if (data.VersionInfo == undefined || data.VersionInfo != "1.0.1.5") {
-		mainContainer.innerHTML = `<font color="#FF0000">Outdated Version Please Update</font>`;
+function GetPlayerPosition(data) {
+	if (HidePlayerPosition)
+	{
 		return;
 	}
-
+	let mainContainer = document.getElementById("srtQueryData");
 	mainContainer.innerHTML += `
 	<div id="position">
 		X: <font color="#00FF00">${data.PlayerPositionX.toFixed(3)}</font>
 		Y: <font color="#00FF00">${data.PlayerPositionY.toFixed(3)}</font>
 		Z: <font color="#00FF00">${data.PlayerPositionZ.toFixed(3)}</font>
 	</div>`;
-	//PLAYERS HP
-	GetPlayerHP(data);
+}
+
+function GetStats(data) {
+	if (HideStats)
+	{
+		return;
+	}
+	let mainContainer = document.getElementById("srtQueryData");
 	mainContainer.innerHTML += `
 	<div id="da">
 		<div class="title">DA Score: </div><font color="#00FF00">${data.RankScore}</font> 
 		<div class="title">DA Rank: </div><font color="#00FF00">${data.Rank}</font>
 		<div class="title">Lei: </div><font color="#00FF00">${data.Lei}</font>
 	</div>`;
-	//GetCurrentEvent(data);
+}
+
+function GetEnemies(data) {
+	if (HideEnemies)
+	{
+		return;
+	}
+	let mainContainer = document.getElementById("srtQueryData");
 	//var table = document.createElement("table");
 	var filterdEnemies = data.EnemyHealth.filter(m => { return (m.IsAlive) });
 	//console.log("Filtered Enemies", filterdEnemies);
@@ -114,4 +151,95 @@ function appendData(data) {
 	});
 	
 	//mainContainer.appendChild(table);
+}
+
+function GetEventType(data) {
+	switch(data.EventType)
+	{
+		case 1:
+			return "Skippable Cutscene";
+		case 2:
+			return "Unskippable Cutscene";
+		case 3:
+			return "Interactable Cutscene";
+		default:
+			return "None";
+	}
+}
+
+function IsCutscene(data) {
+	if (data.IsMotionPlay > 0) {
+		return "True";
+	}
+	return "False";
+}
+
+function GetDebug(data) {
+	if (HideDebug)
+	{
+		return;
+	}
+	let mainContainer = document.getElementById("srtQueryData");
+	mainContainer.innerHTML += `
+	<div id="debug">
+		<div class="title">Event Type: </div><font color="#00FF00">${data.EventType} - ${GetEventType(data)}</font> 
+	</div>
+	<div id="debug">
+		<div class="title">Is Cutscene: </div><font color="#00FF00">${data.IsMotionPlay} - ${IsCutscene(data)}</font> 
+	</div>
+	<div id="debug">
+		<div class="title">Menu State: </div><font color="#00FF00">${GetState(data)}</font> 
+	</div>
+	<div id="debug">
+		<div class="title">Last Key Item: </div><font color="#00FF00">${data.LastKeyItem.ItemName}</font> 
+	</div>
+	`;
+}
+
+function GetState(data) {
+	
+	if (data.PlayerStatus.IsInShop)
+	{
+		//console.log(`IsInShop: ${data.IsInShop}`);
+		return "Shop";
+	}
+	else if (data.PlayerStatus.IsInInventoryMenu)
+	{
+		//console.log(`IsInInventoryMenu: ${data.IsInInventoryMenu}`);
+		return "Inventory Menu";
+	}
+	else if (data.PlayerStatus.IsInSelectMenu)
+	{
+		//console.log(`IsInSelectMenu: ${data.IsInSelectMenu}`);
+		return "Select Menu";
+	}
+	else if (data.PlayerStatus.IsHideShelf)
+	{
+		//console.log(`IsInSelectMenu: ${data.IsInSelectMenu}`);
+		return "Hidden Shelf";
+	}
+	else if (data.PlayerStatus.IsGameOver)
+	{
+		//console.log(`IsInSelectMenu: ${data.IsInSelectMenu}`);
+		return "Game Over";
+	}
+	return "None";
+}
+
+function appendData(data) {
+	//console.log(data);
+	var mainContainer = document.getElementById("srtQueryData");
+	mainContainer.innerHTML = "";
+
+	if (data.VersionInfo == undefined || data.VersionInfo != "1.0.1.5") {
+		mainContainer.innerHTML = `<font color="#FF0000">Outdated Version Please Update</font>`;
+		return;
+	}
+
+	GetPlayerPosition(data);
+	GetPlayerHP(data);	
+	GetStats(data);	
+	GetCurrentEvent(data);
+	GetDebug(data);
+	GetEnemies(data);
 }
