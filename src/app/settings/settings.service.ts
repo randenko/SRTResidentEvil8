24f81bd91c2189ab3bc381126ab0801dee183c1f
Randall Kwiatkowski <randenko@gmail.com>
2021-06-07 23:25:19 -0400

    remove unneeded settings object

--- src/app/settings/settings.service.ts
+++ src/app/settings/settings.service.ts
@@ -11,28 +11,20 @@ import {LocalStorageService} from "../local-storage.service";
 export class SettingsService {
   private defaultSettings: Settings = new Settings(true, false, true, true,
     false, 500, "localhost", 7190);
-  private currentSettings: Settings;
-
   settingsSubject: Subject<Settings>;
 
   constructor(private localStorage: LocalStorageService) {
     this.init();
-    this.settingsSubject = new BehaviorSubject(this.currentSettings);
   }
 
   setSettings(settings: Settings): void {
-    this.currentSettings = settings;
     this.localStorage.set("srtUISettings", settings);
     this.settingsSubject.next(settings);
   }
 
   private init(): void {
-    const settings: Settings = this.localStorage.get("srtUISettings");
-    if (settings) {
-      this.currentSettings = settings;
-    } else {
-      this.currentSettings = this.defaultSettings;
-    }
+    const settings: Settings = this.localStorage.get("srtUISettings") || this.defaultSettings;
+    this.settingsSubject = new BehaviorSubject(settings);
   }
 
 }
