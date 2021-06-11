8954665339adae0e12c7d626443eef6e81aa3fd7
Randall Kwiatkowski <randenko@gmail.com>
2021-06-10 21:53:08 -0400

    implement statistics component

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {Subscription} from "rxjs";
+
+import {GameData} from "../game-host/game-model/game-data";
+import {SrtHostService} from "../game-host/srt-host.service";
 
 @Component({
   selector: 'app-statistics',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.component.css']
 })
 export class StatisticsComponent implements OnInit {
+  gameData: GameData;
+  private gameDataSubscription: Subscription;
 
-  constructor() { }
+  constructor(private srtHostService: SrtHostService) {
+  }
 
   ngOnInit(): void {
+    this.gameDataSubscription = this.srtHostService.getGameData().subscribe(gameDate => {
+      this.gameData = gameDate;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.gameDataSubscription.unsubscribe();
   }
 
 }
