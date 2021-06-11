61c84fb5ab4f6ef4a9656f42549a6481765d93a7
Randall Kwiatkowski <randenko@gmail.com>
2021-06-10 21:36:28 -0400

    implement position component

--- src/app/position/position.component.ts
+++ src/app/position/position.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+
+import {Subscription} from "rxjs";
+
+import {SrtHostService} from "../game-host/srt-host.service";
+import {GameData} from "../game-host/game-model/game-data";
 
 @Component({
   selector: 'app-position',
   templateUrl: './position.component.html',
   styleUrls: ['./position.component.css']
 })
-export class PositionComponent implements OnInit {
+export class PositionComponent implements OnInit, OnDestroy {
+  gameData: GameData;
+  private gameDataSubscription: Subscription;
 
-  constructor() { }
+  constructor(private srtHostService: SrtHostService) {
+  }
 
   ngOnInit(): void {
+    this.gameDataSubscription = this.srtHostService.getGameData().subscribe(gameDate => {
+      this.gameData = gameDate;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.gameDataSubscription.unsubscribe();
   }
 
 }
